1e0104fbacd92783f1b448285cb9a4a98c73ddce
Kemal Yuksel <kemalyksell98@gmail.com>
2022-12-27 00:50:05 +0300

    Initialize

---
+++ spring-api-doc/src/main/java/com/kemalyuksel/SpringApiDocApplication.java
@@ -0,0 +1,14 @@
+package com.kemalyuksel;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringApiDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiDocApplication.class, args);
+    }
+}
